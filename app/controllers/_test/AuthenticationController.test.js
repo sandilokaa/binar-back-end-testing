ab72a620d58b97f9fbce93e0bb9ae0d6f32385e9
sanlokaja <sandyloka790@gmail.com>
2022-06-11 01:24:42 +0700

    materials/undo-auth

--- app/controllers/_test/AuthenticationController.test.js
+++ app/controllers/_test/AuthenticationController.test.js
@@ -3,9 +3,6 @@ const { JWT_SIGNATURE_KEY } = require("../../../config/application");
 const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { User } = require("../../models");
-const request = require('supertest')
-const app = require('../../../app')
-const email = `member${Math.random().toString().substring(12)}@gmail.com`;
 
 
 // ------------------ Authorize ------------------ //
@@ -25,49 +22,6 @@ const email = `member${Math.random().toString().substring(12)}@gmail.com`;
 
 // ------------------ Handle Register ------------------ //
 
-describe('POST /v1/auth/register', () => {
-    it('should response with 201 as status code', async () => {
-        const name = 'sanlokaja'
-        const password = 'sanlokaja1234'
-
-        return request(app)
-            .post('/v1/auth/register')
-            .set('Content-Type', 'application/json')
-            .send({ name, email, password })
-            .then((res) => {
-                expect(res.statusCode).toBe(201)
-                expect(res.body).toEqual(
-                    expect.objectContaining({
-                        ...res.body
-                    })
-                )
-            })
-    })
-
-    it('should response with 422 as status code', async () => {
-        const name = 'Member'
-        const password = 'sanlok12345'
-
-        return request(app)
-            .post('/v1/auth/register')
-            .set('Content-Type', 'application/json')
-            .send({ name, email, password })
-            .then((res) => {
-                expect(res.statusCode).toBe(422)
-                expect(res.body).toEqual(
-                    expect.objectContaining({
-                        error: {
-                            name: expect.any(String),
-                            message: expect.any(String),
-                            details: {
-                                email: expect.any(String)
-                            }
-                        }
-                    })
-                );
-            });
-    });
-});
 
 // ------------------ End Handle Register ------------------ //
 
