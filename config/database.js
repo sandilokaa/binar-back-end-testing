91a9fe96e400ba9838895038e09aea6c10f0f126
sanlokaja <sandyloka790@gmail.com>
2022-06-11 00:42:34 +0700

    materials/fix-database

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
@@ -26,7 +26,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
