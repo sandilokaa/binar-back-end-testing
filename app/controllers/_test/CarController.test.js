a5c1812e57c780bfdde3692d9f8c4628b721a96b
sanlokaja <sandyloka790@gmail.com>
2022-06-10 16:54:55 +0700

    materials/add-car-testing

--- app/controllers/_test/CarController.test.js
+++ app/controllers/_test/CarController.test.js
@@ -4,7 +4,53 @@ const { Car } = require("../../models")
 
 // ------------------ Handle List Cars ------------------ //
 
+// describe('handleListCar', () => {
+//     it('should testing handleListCar function', async () => {
+//         const payloadCar = {
+//             name: "avanza veloz",
+//             price: 359000,
+//             size: "medium",
+//             image: "https://toyotabatangas.com.ph/wp-content/uploads/2019/09/avanza-silver.png",
+//             isCurrentlyRented: false,
+//         };
 
+//         const mockRequest = {
+//             query: {
+//                 page: 1,
+//                 pageSize: 10
+//             }
+//         }
+//         const mockCarModel = {}
+
+//         const mockResponse = {
+//             status: jest.fn().mockReturnThis(),
+//             json: jest.fn().mockReturnThis()
+//         };
+
+//         const app = new CarController({
+//             carModel: mockCarModel
+//         })
+
+//         await app.handleListCars(mockRequest, mockResponse);
+
+//         const mockCar = new Car({
+//             payloadCar
+//         });
+//         mockCarModel.findAll = jest.fn().mockReturnValue(mockCar)
+
+//         const carCount = mockCarModel.count = jest.fn().mockReturnValue({
+//             where: query.where,
+//             include: query.include
+//         })
+
+//         const query = app.getListQueryFromRequest(mockRequest)
+
+//         app.buildPaginationObject(mockRequest, carCount)
+
+//         expect(mockResponse.status).toHaveBeenCalledWith(200);
+//     });
+
+// });
 
 // ------------------ End Handle List Cars ------------------ //
 
@@ -53,4 +99,174 @@ describe("handleGetCar", () => {
 
 
 // ------------------ Handle Create Car ------------------ //
+
+describe('handleCreateCar', () => {
+    it('success result handleCreateCar function', async () => {
+        const payloadCar = {
+            name: "avanza veloz",
+            price: 359000,
+            size: "medium",
+            image: "https://toyotabatangas.com.ph/wp-content/uploads/2019/09/avanza-silver.png",
+            isCurrentlyRented: false,
+        };
+
+        const mockModel = {}
+
+        const mockTest = new Car(payloadCar)
+
+        mockModel.create = jest.fn().mockReturnValue(mockTest)
+
+        const mockResponse = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis(),
+        };
+
+
+        const mockRequest = {
+            body: {
+                payloadCar,
+            }
+        };
+
+        const app = new CarController({
+            carModel: mockModel
+        });
+
+        const hasil = mockModel.create(payloadCar)
+
+        await app.handleCreateCar(mockRequest, mockResponse)
+
+        expect(mockResponse.status).toHaveBeenCalledWith(201);
+        expect(mockResponse.json).toHaveBeenCalledWith(hasil);
+    });
+
+    it('error handleCreateCar function', async () => {
+        const err = new Error("not Found!");
+
+        const payloadCar = {
+            name: "avanza veloz",
+            price: 359000,
+            size: "medium",
+            image: "https://toyotabatangas.com.ph/wp-content/uploads/2019/09/avanza-silver.png",
+            isCurrentlyRented: false,
+        };
+
+        const mockModel = {}
+
+        new Car(payloadCar)
+
+        mockModel.create = jest.fn().mockReturnValue(Promise.reject(err))
+
+        const mockResponse = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis(),
+        };
+
+
+        const mockRequest = {
+            body: {
+                payloadCar,
+            }
+        };
+
+        const app = new CarController({
+            carModel: mockModel
+        });
+
+        mockModel.create(payloadCar)
+
+        await app.handleCreateCar(mockRequest, mockResponse)
+
+        expect(mockResponse.status).toHaveBeenCalledWith(422);
+        expect(mockResponse.json).toHaveBeenCalledWith({
+            error: {
+                name: err.name,
+                message: err.message,
+            }
+        });
+    });
+});
+
 // ------------------ End Handle Create Car ------------------ //
+
+
+// ------------------ Get Car From Request ------------------ //
+
+describe('getCarFromRequest', () => {
+    it('should testing getCarFromRequest function', async () => {
+        const payloadCar = {
+            name: "avanza veloz",
+            price: 359000,
+            size: "medium",
+            image: "https://toyotabatangas.com.ph/wp-content/uploads/2019/09/avanza-silver.png",
+            isCurrentlyRented: false,
+        };
+
+        const mockRequest = {
+            params: {
+                id: 1
+            }
+        }
+
+        const mockCar = new Car({
+            payloadCar
+        })
+        const mockCarModel = {}
+        mockCarModel.findByPk = jest.fn().mockReturnValue(mockCar)
+
+        const mockResponse = {};
+        mockResponse.status = jest.fn().mockReturnThis();
+        mockResponse.json = jest.fn().mockReturnThis();
+
+        const carcontroller = new CarController({
+            carModel: mockCarModel
+        });
+        const result = await carcontroller.getCarFromRequest(mockRequest);
+
+        expect(result).toStrictEqual(mockCar)
+    });
+});
+
+// ------------------ End Get Car From Request ------------------ //
+
+
+// ------------------ Handle Update Car ------------------ //
+
+describe('handleUpdateCar', () => {
+    it('should testing handleUpdateCar function', async () => {
+        const mockRequest = {};
+        const {
+            name,
+            price,
+            size,
+            image,
+        } = mockRequest.body;
+
+        const mockCarModel = {};
+        const mockResponse = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        }
+
+        const app = new CarController({
+            carModel: mockCarModel
+        })
+
+        const car = await app.getCarFromRequest(mockRequest);
+
+        await car.update({
+            name,
+            price,
+            size,
+            image,
+            isCurrentlyRented: false
+        })
+
+        await app.handleUpdateCar(mockRequest, mockResponse);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json), toHaveBeenCalledWith(car);
+    });
+});
+
+// ------------------ End Handle Update Car ------------------ //
