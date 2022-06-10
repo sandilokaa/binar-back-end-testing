dffff4da3d50294e22fefaab065eba1c210e01ed
sanlokaja <sandyloka790@gmail.com>
2022-06-11 01:01:59 +0700

    materials/register-test

--- app/controllers/_test/AuthenticationController.test.js
+++ app/controllers/_test/AuthenticationController.test.js
@@ -3,6 +3,9 @@ const { JWT_SIGNATURE_KEY } = require("../../../config/application");
 const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { User } = require("../../models");
+const request = require('supertest')
+const app = require('../../../app')
+const email = `member${Math.random().toString().substring(12)}@gmail.com`;
 
 
 // ------------------ Authorize ------------------ //
@@ -12,9 +15,59 @@ const { User } = require("../../models");
 // ------------------ End Authorize ------------------ //
 
 
+// ------------------ Handle Login ------------------ //
+
+
+
+// ------------------ End Handle Login ------------------ //
+
+
+
 // ------------------ Handle Register ------------------ //
 
+describe('POST /v1/auth/register', () => {
+    it('should response with 201 as status code', async () => {
+        const name = 'sanlokaja'
+        const password = 'sanlokaja1234'
 
+        return request(app)
+            .post('/v1/auth/register')
+            .set('Content-Type', 'application/json')
+            .send({ name, email, password })
+            .then((res) => {
+                expect(res.statusCode).toBe(201)
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        ...res.body
+                    })
+                )
+            })
+    })
+
+    it('should response with 422 as status code', async () => {
+        const name = 'sanlok'
+        const password = 'sanlok12345'
+
+        return request(app)
+            .post('/v1/auth/register')
+            .set('Content-Type', 'application/json')
+            .send({ name, email, password })
+            .then((res) => {
+                expect(res.statusCode).toBe(422)
+                expect(res.body).toEqual(
+                    expect.objectContaining({
+                        error: {
+                            name: expect.any(String),
+                            message: expect.any(String),
+                            details: {
+                                email: expect.any(String)
+                            }
+                        }
+                    })
+                );
+            });
+    });
+});
 
 // ------------------ End Handle Register ------------------ //
 
