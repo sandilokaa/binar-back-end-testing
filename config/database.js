9d79d9a82027502e5a1740e3b89f881b4885fb4c
sanlokaja <sandyloka790@gmail.com>
2022-06-11 11:01:06 +0700

    materials/add-port

--- config/database.js
+++ config/database.js
@@ -16,11 +16,18 @@ module.exports = {
     },
   },
   test: {
-    username: "postgres",
-    password: "sanlokaja123",
-    database: "instagram_db",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: process.env.DB_USER,
